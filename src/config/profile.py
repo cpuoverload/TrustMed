323728482fca9e650cf97325c7f6032f597d6b4a
cpuoverload <singhainanjifan@outlook.com>
2025-03-05 17:59:01 +0800

    feat: add new profile

--- src/config/profile.py
+++ src/config/profile.py
@@ -73,6 +73,18 @@ SERVER_EVALUATION_PROFILES: list[ProfileType] = [
         "query_rewrite_num": 3,
     },
     {
+        "profile_name": "hybrid_search_rerank",
+        "collection_name": "hybrid_search_rerank",
+        "embedding_model": hf_bge_large_embedding,
+        "chunk_size": 512,
+        "chunk_overlap": 50,
+        "top_k": 6,
+        "llm": hf_llama_8b_llm,
+        "hybrid_search": True,
+        "reranker": bge_rerank_large,
+        "reranker_top_n": 6,
+    },
+    {
         "profile_name": "query_rewrite_rerank",
         "collection_name": "query_rewrite_rerank",
         "embedding_model": hf_bge_large_embedding,
